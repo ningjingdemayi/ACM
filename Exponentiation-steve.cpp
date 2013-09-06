bf54a28134ca1051bfa67f3f64182eaabec9d70e
Steve <914574188@qq.com>
2013-09-06 15:45:45 +0800

    update problem 2.
    It is workable now.

--- Exponentiation-steve.cpp
+++ Exponentiation-steve.cpp
@@ -1,7 +1,6 @@
 #include "stdafx.h"
 #include <vector>
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -24,6 +23,11 @@ int toInt(char val)
 	return val - 48;
 }
 
+char toChar(int val)
+{
+	return val + 48;
+}
+
 string add(string v1, string v2)
 {
 	string t;
@@ -33,24 +37,30 @@ string add(string v1, string v2)
 		v1 = v2;
 		v2 = t;
 	}
-
+	
 	int up = 0;
-	int i = 0;
-	for(i ; i< v1.size(), i<=v2.size(); i++)
+	int i;
+	for(i = 0; i < v2.size(); i++)
 	{
-		int t1 = toInt(v1[i]);
-		int t2 = toInt(v2[i]);
+		int t1 = toInt(v1[v1.size() - i - 1]);
+		int t2 = toInt(v2[v2.size() - i - 1]);
 		int t = t1 + t2 + up;
-		up = t > 10 ? 1 : 0;
-		v1[i] = t % 10;
+		up = t >= 10 ? 1 : 0;
+		v1[v1.size() - i - 1] = toChar(t % 10);
 	}
 
-	if(up) v1[i] = toInt(v1[i])+1;
+	if(up) 
+	{
+		if(v1.size() > i)
+			v1[v1.size() - i - 1] = toChar(toInt(v1[v1.size() - i - 1])+1);
+		else
+			v1 = "1" + v1; 
+	}
 
 	return v1;
 }
 
-void comput(string *num)
+string comput(string num, string num2)
 {
 	vector<number> calc;
 	vector<string> result;
@@ -59,39 +69,50 @@ void comput(string *num)
 	vector<int> nums;
 	long tmp;
 
-	for(int i=0; i < (*num).size(); i++)
+	if(num.size() < num2.size())
 	{
-		nums.push_back(toInt((*num)[i]));
+		bits = num;
+		num = num2;
+		num2 = bits;
+		bits = "";
 	}
 
-	while((*num).size() >5)
+	for(int i=0; i < num2.size(); i++)
 	{
-		calc.push_back(number((*num).substr(0, 5)));
-		*num = (*num).substr(5,(*num).size());
+		nums.push_back(toInt(num2[i]));
 	}
-	calc.push_back(number((*num).substr(0, (*num).size())));
 
-	for(auto p = nums.end(); p != nums.begin(); p-- )
+	while(num.size() >5)
 	{
-		for(int i = 0; i< calc.size(); i++)
+		calc.push_back(number(num.substr(0, 5)));
+		num = num.substr(5,num.size());
+	}
+	calc.push_back(number(num.substr(0, num.size())));
+
+	for(int p = nums.size() - 1; p >= 0; p-- )
+	{
+		for(int i = calc.size() - 1; i >= 0; i--)
 		{
 			char buffer[10];
-			tmp = (*p) * calc[i].numValue;
+			tmp = nums[p] * calc[i].numValue;
+			if(tmp == 0) continue;
 			_ltoa_s(tmp, buffer, 10, 10);
 			result.push_back(string(buffer) + bits);
 			bits += string(calc[i].length, '0');
 		}
 
-		bits = "";
+		bits = string(nums.size() - p, '0');
 	}
 
 	//Add results one by one.
 
 	string finalRes = result[0];
-	for(int i = 1; i < calc.size(); i++)
+	for(int i = 1; i < result.size(); i++)
 	{
 		finalRes = add(finalRes, result[i]);
 	}
+
+	return finalRes;
 }
 
 int Problem2()
@@ -99,9 +120,7 @@ int Problem2()
 	string numString;
 	int times, dotpos;
 
-	//getline(cin,numString);
-
-	numString = "1.01234 3";
+	getline(cin,numString);
 
 	times =	atoi(numString.substr(numString.find_first_of(' ', 0) + 1, numString.size()).c_str());
 	numString = numString.substr(0, numString.find_first_of(' ', 0));
@@ -112,8 +131,13 @@ int Problem2()
 
 	dotpos *= times;
 	
-	for(int i = 0; i< times ; i++)
-		comput(&numString);
+	string res = numString;
+
+	for(int i = 0; i< times -1 ; i++)
+		res = comput(res, numString);
 	
+	res = res.substr(0, res.size() - dotpos) + "." + res.substr(res.size() - dotpos, res.size());
+
+	cout << "final: " << res << endl;
     return 0;
 }
